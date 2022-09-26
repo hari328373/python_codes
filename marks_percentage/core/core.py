8c53cf0ac016b6af9b62a35319e5c02d6b7101b8
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-09-26 18:27:00 +0530

    percentage of marks in dictionary datatype

---
+++ marks_percentage/core/core.py
@@ -0,0 +1,9 @@
+# The average of the marks obtained by the particular student correct to 2 decimal places
+
+def percentage(student_marks,query_name):
+    total = 0
+    for i in range(len(student_marks[query_name])):
+       total = total + student_marks[query_name][i] #adding marks of query name
+    avg = total /len(student_marks[query_name])
+    return  avg
+#print(percentage_marks(student_marks={'harsh': [25, 26.5, 28], 'Anurag': [26, 28, 30]},query_name='harsh'))
